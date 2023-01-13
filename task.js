20df11008b987b195ea591e3b422d7a2409277b1
GauravPoosarla <poosarlagaurav@gmail.com>
2023-01-13 15:35:20 +0530

    feat: add prime and armstrong checking capability

---
+++ task.js
@@ -0,0 +1,35 @@
+function isPrime(x)
+{
+    for(let i=2; i<=Math.sqrt(x); i++)
+    {
+        if(x%i == 0)
+            return false;
+    }
+    return true;
+}
+
+function isArmstrong(x)
+{
+    let sum = 0;
+    const numDigits = x.length;
+
+    let temp = x;
+
+    while(temp > 0)
+    {
+        let lastDigit = temp%10;
+        sum += lastDigit ** numDigits;
+        temp = parseInt(temp / 10);
+    }
+
+    if(sum == x)
+        return true;
+    else
+        return false;
+}
+
+console.log(isPrime(20));
+console.log(isPrime(7));
+
+console.log(isArmstrong('153'));
+console.log(isArmstrong('50'));
